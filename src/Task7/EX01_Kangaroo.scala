319721b86bed6775da2903a1b27193074d98afcb
Sovichea Nhim <nhimsovichea18@kit.edu.kh>
2019-09-17 01:42:24 +0700

    Refactor Task 7

--- src/Task7/EX01_Kangaroo.scala
+++ src/Task7/EX01_Kangaroo.scala
@@ -5,28 +5,17 @@ import util.control.Breaks._
 object EX01_Kangaroo {
   def kangaroo():Unit = {
     println("Enter the first kangaroo's location and speed and the second's respectively : ")
-    val input = readLine().split(" ")
-    val kangaroo = input.map(_.toInt)
-    var jumps = 0
+    val kangaroo = readLine().split(" ").map(_.toInt)
     breakable{
-      while(true){
+      while(kangaroo(0)<kangaroo(2)){
         kangaroo(0) += kangaroo(1)
         kangaroo(2) += kangaroo(3)
-        jumps += 1
-//        println(kangaroo(0), kangaroo(2))
         if(kangaroo(0) == kangaroo(2)){
-          println(jumps)
-          println(kangaroo(0), kangaroo(2))
           println("YES")
           break
         }
-        else if(jumps < 0 || kangaroo(0) > 100000000 || kangaroo(2) > 100000000) {
-          println(kangaroo(0), kangaroo(2))
-          println(jumps)
-          println("NO")
-          break
-        }
       }
+      println("NO")
     }
   }
   def main(args: Array[String]): Unit = {
