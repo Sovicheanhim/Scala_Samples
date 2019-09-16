10da3ba6b2d44d583fa2275e7bd850e6d36a6aaf
Sovichea Nhim <nhimsovichea18@kit.edu.kh>
2019-09-17 02:12:42 +0700

    Reafactor Task 7

--- src/Task6/EX01_Pages.scala
+++ src/Task6/EX01_Pages.scala
@@ -6,9 +6,9 @@ object EX01_Pages {
     println("Enter the number of pages and the page you want to open respectively :")
     val (totalPages, page) = (readInt(), readInt())
     val pagesTurned = if (totalPages%2 == 0)
-      if ((totalPages - page)/2.0 < Math.ceil((page - 1)/2.0).toInt ) Math.ceil((totalPages - page)/2.0).toInt else Math.ceil((page - 1)/2.0).toInt
+      Math.min(Math.ceil((totalPages - page)/2.0).toInt,Math.ceil((page - 1)/2.0).toInt)
     else
-      if ((totalPages - page)/2.0 < Math.ceil((page - 1)/2.0).toInt ) Math.floor((totalPages - page)/2.0).toInt else Math.ceil((page - 1)/2.0).toInt
+      Math.min(Math.floor((totalPages - page)/2.0).toInt, Math.ceil((page - 1)/2.0).toInt)
     println(s"The minimum pages needed is $pagesTurned")
   }
 }
--- src/Task7/EX01_Kangaroo.scala
+++ src/Task7/EX01_Kangaroo.scala
@@ -6,17 +6,7 @@ object EX01_Kangaroo {
   def kangaroo():Unit = {
     println("Enter the first kangaroo's location and speed and the second's respectively : ")
     val kangaroo = readLine().split(" ").map(_.toInt)
-    breakable{
-      while(kangaroo(0)<kangaroo(2)){
-        kangaroo(0) += kangaroo(1)
-        kangaroo(2) += kangaroo(3)
-        if(kangaroo(0) == kangaroo(2)){
-          println("YES")
-          break
-        }
-      }
-      println("NO")
-    }
+    if (kangaroo(1) > kangaroo (3) && ((kangaroo(2)-kangaroo(0))%(kangaroo(1)-kangaroo(3)) == 0)) println("YES") else println("NO")
   }
   def main(args: Array[String]): Unit = {
     kangaroo()
