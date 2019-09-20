cb76e39b06209b7af37d334359fa57a4891be8ce
Sovichea Nhim <nhimsovichea18@kit.edu.kh>
2019-09-21 04:57:14 +0700

    Refactor Task9

--- src/Task9/EX01_Electronics_Shop.scala
+++ src/Task9/EX01_Electronics_Shop.scala
@@ -1,27 +1,21 @@
 package Task9
-import scala.collection
-import scala.collection.mutable
 import scala.io.StdIn._
-import scala.collection.mutable._
-
 
 object EX01_Electronics_Shop {
   def main(args: Array[String]): Unit = {
     println("Enter the amount of money, number of keyboard brands and number of USB drive brands respectively :")
-    val input = readLine().split(" ").map(_.toInt)
+    val input = readLine().split(" ").map(_.trim.toInt)
     var (money, keyboard, usbDriver, max) = (input(0), input(1), input(2), -1)
-    val n = checkCase(keyboard)
-    val m = checkCase(usbDriver)
-//    println(n.mkString(" "), m.mkString(" "))
+    val (n, m) = (checkCase(keyboard), checkCase(usbDriver))
     n.foreach{ i=>{ m.foreach{ j=>{if(i+j>=max&&i+j<=money) max = i+j} } } }
     println(max)
   }
   def checkCase(count:Int):Array[Int] = {
-    var n = readLine().split(" ").map(_.toInt)
-    if(!(n.size==count)){
+    var input = readLine().split(" ").map(_.toInt)
+    if(!(input.size==count)){
       println(s"Please enter the input of $count integers again")
-      n = readLine().split(" ").map(_.toInt)
+      input = readLine().split(" ").map(_.toInt)
     }
-    n
+    input
   }
 }
